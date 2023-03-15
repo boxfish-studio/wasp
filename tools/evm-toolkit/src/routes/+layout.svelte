b9938a1c639ac93a72cf5d714581d0e7fa1ca00b
evavirseda <evirseda@boxfish.studio>
2023-03-15 11:31:48 +0100

    fix: background

--- tools/evm-toolkit/src/routes/+layout.svelte
+++ tools/evm-toolkit/src/routes/+layout.svelte
@@ -4,6 +4,23 @@
 </script>
 
 <Navbar />
-<main class="w-full h-full flex items-center justify-center">
+<main class="w-full flex items-center justify-center">
+  <background-radial />
   <slot />
 </main>
+
+<style lang="scss">
+  background-radial {
+    @apply absolute;
+    @apply top-0;
+    @apply w-full;
+    @apply h-full;
+    @apply z-[-1];
+    background: radial-gradient(
+      farthest-corner at top right,
+      #00e0c7 -44%,
+      #061928 40%,
+      #061928 100%
+    );
+  }
+</style>
