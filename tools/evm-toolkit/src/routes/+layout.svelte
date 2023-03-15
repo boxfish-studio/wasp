58486488fbdfe95cc389055c70f890294c4c0ab0
evavirseda <evirseda@boxfish.studio>
2023-03-15 11:04:30 +0100

    feat: add box component

--- tools/evm-toolkit/src/routes/+layout.svelte
+++ tools/evm-toolkit/src/routes/+layout.svelte
@@ -1,10 +1,9 @@
 <script lang="ts">
   import { Navbar } from '$components';
-import '../app.scss'
- 
+  import '../app.scss';
 </script>
 
 <Navbar />
-<main class="w-full h-full">
+<main class="w-full h-full flex items-center justify-center">
   <slot />
 </main>
