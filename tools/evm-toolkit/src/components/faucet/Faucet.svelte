469159ebd3ae5327a670fbfc14f6813a71ba7c26
evavirseda <evirseda@boxfish.studio>
2023-03-16 11:15:11 +0100

    fix: leave Faucet component as it was

--- tools/evm-toolkit/src/components/faucet/Faucet.svelte
+++ tools/evm-toolkit/src/components/faucet/Faucet.svelte
@@ -5,48 +5,36 @@
   import { toast } from '@zerodevx/svelte-toast';
   import { selectedNetwork, nodeClient, indexerClient } from '../../store';
   import { Bech32AddressLength, EVMAddressLength } from '../../lib/constants';
-  import { Input } from '..';
-  import Button from '../button.svelte';
-
   let isSendingFunds: boolean;
   let errorMessage: string;
-
   let balance: bigint = BigInt(0);
   let evmAddress: string = '';
-
   $: enableSendFunds =
     evmAddress.length == EVMAddressLength &&
     $selectedNetwork != null &&
     $selectedNetwork.chainAddress.length == Bech32AddressLength &&
     !isSendingFunds;
-
   async function sendFunds() {
     if (!enableSendFunds) {
       return;
     }
-
     errorMessage = undefined;
     isSendingFunds = true;
-
     let wallet: IotaWallet = new IotaWallet(
       $nodeClient,
       $indexerClient,
       $selectedNetwork.faucetEndpoint,
     );
-
     let toastId: number;
-
     try {
       toastId = toast.push('Initializing wallet');
       await wallet.initialize();
       toast.pop(toastId);
-
       toastId = toast.push('Requesting funds from the faucet', {
         duration: 20 * 2000, // 20 retries, 2s delay each.
       });
       balance = await wallet.requestFunds();
       toast.pop(toastId);
-
       toastId = toast.push('Sending funds');
       const transaction = new SendFundsTransaction(wallet);
       await transaction.sendFundsToEVMAddress(
@@ -56,7 +44,6 @@
         BigInt(5000000),
       );
       toast.pop(toastId);
-
       toast.push(
         'Funds successfully sent! It may take 10-30 seconds to arive.',
         {
@@ -68,38 +55,39 @@
       toast.pop(toastId);
       toast.push(ex.message);
     }
-
     isSendingFunds = false;
   }
 </script>
 
-<faucet-component class="flex flex-col space-y-6 mt-6">
+<component>
   {#if $selectedNetwork}
-    <Input
-      id="evmAddress"
-      label="Your EVM Address"
-      bind:value={evmAddress}
-      stretch
-    />
+    <div class="input_container">
+      <span class="header">Your EVM Address</span>
+      <input type="text" bind:value={evmAddress} />
+    </div>
 
-    <!-- TODO: when we add notification manager we should replace this with a notification. -->
     {#if errorMessage}
-      <div class="error">
-        <div class="error_title">Error</div>
-        <div class="error_message">
-          {errorMessage}
+      <div class="input_container">
+        <div class="error">
+          <div class="error_title">Error</div>
+          <div class="error_message">
+            {errorMessage}
+          </div>
         </div>
       </div>
     {/if}
 
-    <Button
-      title="Send funds"
-      disabled={!enableSendFunds}
-      onClick={sendFunds}
-      busy={isSendingFunds}
-    />
+    <div class="input_container">
+      <button class="button" disabled={!enableSendFunds} on:click={sendFunds}>
+        {#if !isSendingFunds}
+          Send funds
+        {:else}
+          Sending ..
+        {/if}
+      </button>
+    </div>
   {/if}
-</faucet-component>
+</component>
 
 <style>
   .error {
@@ -108,12 +96,10 @@
     border-radius: 10px;
     padding: 15px;
   }
-
   .error_title {
     font-weight: bold;
     margin-bottom: 15px;
   }
-
   component {
     color: rgba(255, 255, 255, 0.87);
     display: flex;
