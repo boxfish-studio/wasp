b3f75c8240d5bcddef7a4bef4703de1b5cc1d0b6
evavirseda <evirseda@boxfish.studio>
2023-03-17 10:38:00 +0100

    feat: polish network settings

--- tools/evm-toolkit/src/components/popup/settings-popup.svelte
+++ tools/evm-toolkit/src/components/popup/settings-popup.svelte
@@ -1,8 +1,41 @@
 <script lang="ts">
-    import type { ComponentType } from "svelte";
-  export let component: ComponentType
+  import { Input, Select } from '$components';
+  import { selectedNetwork, networks } from '../../store';
+
 </script>
 
-<slot>
-  <svelte:component this={component} />
-</slot>
+<network-settings-component class="flex flex-col space-y-6">
+  {#if $selectedNetwork}
+    <Select
+      options={$networks}
+      displayValue={network => network.text}
+      bind:value={$selectedNetwork}
+    />
+    <Input
+      id="hornetEndpoint"
+      label="Hornet API endpoint"
+      bind:value={$selectedNetwork.apiEndpoint}
+      disabled={$selectedNetwork.id !== 1}
+      stretch
+    />
+    <Input
+      id="faucetEndpoint"
+      label="Faucet API endpoint"
+      bind:value={$selectedNetwork.faucetEndpoint}
+      disabled={$selectedNetwork.id !== 1}
+      stretch
+    />
+    <Input
+      id="chainAddress"
+      label="Chain Address"
+      bind:value={$selectedNetwork.chainAddress}
+      disabled={$selectedNetwork.id !== 1}
+      stretch
+    />
+  {:else}
+    <span>Loading Network Config File...</span>
+  {/if}
+</network-settings-component>
+
+<style lang="scss">
+</style>
