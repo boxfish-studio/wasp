8f6ab84ed6b26770747d6f4296b6b245e02e3b9e
evavirseda <evirseda@boxfish.studio>
2023-03-17 11:52:46 +0100

    feat. add persistent store

--- tools/evm-toolkit/src/store.ts
+++ tools/evm-toolkit/src/store.ts
@@ -1,23 +1,29 @@
-import { writable } from 'svelte/store';
+import { type Writable, writable } from 'svelte/store';
 import type { NetworkOption } from '$lib/network_option';
 import { SingleNodeClient, IndexerPluginClient } from '@iota/iota.js';
 import { BrowserPowProvider } from '@iota/pow-browser.js';
+import { persistent } from '$lib/stores';
+
+const SELECTED_NETWORK_KEY = 'selectedNetwork';
 
 export const networks = writable<NetworkOption[]>([]);
-export const selectedNetwork = writable<NetworkOption>();
-
-selectedNetwork.subscribe(network => {
-  if (!network) {
-    return;
-  }
-
-  console.log(`Creating new client for: ${network.apiEndpoint}`);
-  const client = new SingleNodeClient(network.apiEndpoint, {
-    powProvider: new BrowserPowProvider(),
-  });
-  nodeClient.set(client);
-  indexerClient.set(new IndexerPluginClient(client));
-});
+export const selectedNetwork: Writable<NetworkOption> = persistent(
+  SELECTED_NETWORK_KEY,
+  null,
+);
 
 export const indexerClient = writable<IndexerPluginClient>();
 export const nodeClient = writable<SingleNodeClient>();
+
+selectedNetwork?.subscribe(network => {
+  if (!network) {
+    return;
+  } else {
+    console.log(`Creating new client for: ${network?.apiEndpoint}`);
+    const client = new SingleNodeClient(network.apiEndpoint, {
+      powProvider: new BrowserPowProvider(),
+    });
+    nodeClient?.set(client);
+    indexerClient?.set(new IndexerPluginClient(client));
+  }
+});
